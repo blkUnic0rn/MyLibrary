98903febf8f60184ef7bd7d5b29d87eec4676d75
Kayla-SA-Williams <shalisha.angelique@gmail.com>
2020-01-29 11:01:15 -0500

    Change 'int' in migrate file
    1.change 'int' to integer in 20200129153221_add_fields_to_user.rb

--- db/migrate/20200129153221_add_fields_to_user.rb
+++ db/migrate/20200129153221_add_fields_to_user.rb
@@ -3,7 +3,7 @@ class AddFieldsToUser < ActiveRecord::Migration[5.2]
     add_column :users, :given_name, :string
     add_column :users, :surname, :string
     add_column :users, :dob, :date
-    add_column :users, :book_count, :int
+    add_column :users, :book_count, :integer
     add_column :users, :reader_status, :string
   end
 end
