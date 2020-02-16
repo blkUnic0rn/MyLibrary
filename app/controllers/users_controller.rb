722a8ece5a9ae4a1d0bd7662f12dc9d842996b2c
Kayla-SA-Williams <shalisha.angelique@gmail.com>
2020-02-16 01:35:41 -0500

    Correct spelling error on users controller
    give_name to given_name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,7 +51,7 @@ class UsersController < ProtectedController
   def user_creds
     params.require(:credentials)
           .permit(:email, :password, :password_confirmation,
-                  :give_name, :surname, :dob)
+                  :given_name, :surname, :dob)
   end
 
   def pw_creds
